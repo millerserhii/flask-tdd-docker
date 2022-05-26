48d61af96934781ebec825def5c1d05f07ce46fc
millerserhii <millerserhii@gmail.com>
2022-05-26 19:47:41 +0200

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app.config.from_object('src.config.DevelopmentConfig')
+
+
+class Ping(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+api.add_resource(Ping, '/ping')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
